477f905d9d46754378dc1af8197469b792cc930e
jhk01007 <jhk001007@naver.com>
2025-11-20 18:06:13 +0900

    feat(SummaryPage.jsx): 요약 페이지 임시 추가

---
+++ src/pages/SummaryPage.jsx
@@ -0,0 +1,7 @@
+export default function SummaryPage() {
+  return (
+    <div className="flex h-[60vh] items-center justify-center">
+      <p className="text-xl text-neutral-500">아직 개발 중입니다.</p>
+    </div>
+  );
+}
